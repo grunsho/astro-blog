75d6418d077b4a00978bff5682706d6fef9d5eff
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-01-11 20:17:27 -0300

    added SSR and deploy to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: squooshImageService()
   },
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  output: 'server',
+  adapter: netlify()
 });
