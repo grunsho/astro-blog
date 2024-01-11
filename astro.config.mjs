c60f4ee08b4c401d5095ff3dda39dfc4d2487f01
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-01-11 19:49:19 -0300

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig, squooshImageService } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    service: squooshImageService()
+  },
+  integrations: [tailwind(), mdx()]
+});
